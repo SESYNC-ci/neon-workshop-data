62287b1baaf3e2ce8df65272ac5d641584bd8e85
Andrew Elmore <aelmore@rstudio03.research.sesync.org>
2020-01-07 14:14:00 -0500

    First attempt to get ACS data for all geoids

--- getNEONcensusdata.R
+++ getNEONcensusdata.R
@@ -7,6 +7,18 @@ library(tidyr)
 library(foreign)
 library(sf)
 
+#######################################################
+# Retrive fips codes 
+#######################################################
+data_dir <- "/nfs/public-data/NEON_workshop_data/NEON"
+NEONgeoids <-  readr::read_csv(file.path(data_dir, "NEON-AOP-CensusGEOIDs.csv"), col_types = "cccccccc")
+state.use = unique(NEONgeoids$STATEFP)
+
+#######################################################
+# Retrive ACS dataset names and codes
+#######################################################
+ACScodes <-  readr::read_csv(file.path(data_dir, "NEON-AOP-ACSdatasets.csv"), col_types = "cc")
+
 ########### Downloading Census Data ######################
 # must set up .Renviron file with an API key requested from here:
 # https://api.census.gov/data/key_signup.html
@@ -26,14 +38,38 @@ census_api_key(apikey, install = TRUE, overwrite=TRUE)
 readRenviron("~/.Renviron")
 Sys.getenv("CENSUS_API_KEY")
 
-#This example gets population data for state 4, count 1, which is Apache County, Arizona
-#I think the plan will be to read in a list of states and counties overlaping the AOP footprints 
-population <- get_acs(geography = "tract", 
-                              variables = "B01003_001",
-                              state = 4, county = 1,
-                              geometry = FALSE, survey = "acs5")
+#This example gets population data for state.use, a vector of all unique states with a NEON site
+#I think the plan will be to read in a list of states (and mabye counties) overlaping the AOP footprints 
+temp = "B01003001"
 
 
+
+GetOneDataset = function(temp, state.use, NEONgeoids){
+  ACSdataset = paste0(substr(temp,1,6),"_",substr(temp,7,9))
+  df <- get_acs(geography = "tract", 
+                        variables = ACSdataset,
+                        state = state.use,
+                        geometry = FALSE, survey = "acs5")
+  
+  NEON.ACS.one = left_join(NEONgeoids,df[,c(1,4)])
+  names(NEON.ACS.one)[which(names(NEON.ACS.one) == "estimate")] = ACSdataset
+
+  return(NEON.ACS.one[,which(names(NEON.ACS.one) == ACSdataset)])
+}
+
+NEON.ACS = NEONgeoids
+for(temp in ACScodes[,1]){
+  NEON.ACS.one = GetOneDataset(temp, state.use, NEONgeoids)
+  NEON.ACS = cbind(NEON.ACS,NEON.ACS.one)
+}
+
+write.csv(NEON.ACS,file.path(data_dir, "NEON-AOP-ACS.csv"))
+
+########################################
+# a bunch of notes on the ACS. This is all found in the NEON-AOP-ACSdatasets.csv file.
+###############################################
+
+#B01003001 Population
 #Race related variables
 #https://www.socialexplorer.com/data/ACS2017_5yr/metadata/?ds=ACS17_5yr&table=B02001
 #B02001002 White Alone 
@@ -88,20 +124,6 @@ population <- get_acs(geography = "tract",
 #B17001002 Income in the Past 12 Months Below Poverty Level:
 #B17001031 Income in the Past 12 Months At or Above Poverty Level:
 
-allpovertydata <- get_acs(geography = "tract", 
-                      variables = "B17001_001",
-                      state = 4, county = 1,
-                      geometry = FALSE, survey = "acs5")
-
-belowpoverty <- get_acs(geography = "tract", 
-                      variables = "B17001_002",
-                      state = 4, county = 1,
-                      geometry = FALSE, survey = "acs5")
-
-abovepoverty <- get_acs(geography = "tract", 
-                   variables = "B17001_031",
-                   state = 4, county = 1,
-                   geometry = FALSE, survey = "acs5")
 
 #Transportation related variables
 # 
